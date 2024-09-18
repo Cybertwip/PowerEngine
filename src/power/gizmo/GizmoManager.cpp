0c6b9710f8f4580ed0649bd8dc10341bcd1012f9
Kai <greentwip@gmail.com>
2024-09-18 17:57:11 +0200

    Improve gizmos

--- src/power/gizmo/GizmoManager.cpp
+++ src/power/gizmo/GizmoManager.cpp
@@ -27,7 +27,7 @@ GizmoManager::GizmoManager(nanogui::Widget& parent, ShaderManager& shaderManager
 mMeshActorLoader(meshActorLoader),
 mShader(std::make_unique<ShaderWrapper>(*shaderManager.get_shader("gizmo"))),
 mTranslationGizmo(mMeshActorLoader.create_actor("models/Gizmo/Translation.fbx", *mShader)),
-mRotationGizmo(mMeshActorLoader.create_actor("models/Gizmo/Rotation.fbx"), *mShader)
+mRotationGizmo(mMeshActorLoader.create_actor("models/Gizmo/Rotation.fbx", *mShader)
 /*mScaleGizmo(mMeshActorLoader.create_actor("models/Gizmo/Translation.fbx"))*/ {
 	
 	// Translation Button
