4c79a4e06944897c799b2969e25c06a5f5f40afd
Kai <greentwip@gmail.com>
2024-09-19 23:12:47 +0200

    Fix textbox

--- external/nanogui/src/textbox.cpp
+++ external/nanogui/src/textbox.cpp
@@ -380,9 +380,7 @@ bool TextBox::focus_event(bool focused) {
 bool TextBox::keyboard_event(int key, int  scancode, int action, int modifiers) {
 	if (m_editable && focused()) {
 		if (action == GLFW_PRESS || action == GLFW_REPEAT) {
-			
-			bool is_focus_change = false;
-			
+						
 			if (key == GLFW_KEY_LEFT) {
 				if (modifiers == GLFW_MOD_SHIFT) {
 					if (m_selection_pos == -1)
@@ -436,7 +434,6 @@ bool TextBox::keyboard_event(int key, int  scancode, int action, int modifiers) 
 			} else if (key == GLFW_KEY_ENTER) {
 				if (!m_committed)
 				{
-					is_focus_change = true;
 					focus_event(false);
 				}
 			} else if (key == GLFW_KEY_A && modifiers == SYSTEM_COMMAND_MOD) {
@@ -455,9 +452,6 @@ bool TextBox::keyboard_event(int key, int  scancode, int action, int modifiers) 
 			m_valid_format =
 			(m_value_temp == "") || check_format(m_value_temp, m_format);
 			
-			if (m_valid_format && !is_focus_change && glfwGetKeyName(key, scancode) != nullptr){
-				m_callback(m_value_temp + std::string{glfwGetKeyName(key, scancode)});
-			}
 		}
 		
 		return true;
@@ -477,6 +471,10 @@ bool TextBox::keyboard_character_event(unsigned int codepoint) {
 		
 		m_valid_format = (m_value_temp == "") || check_format(m_value_temp, m_format);
 		
+		if (m_valid_format && m_callback){
+			m_callback(m_value_temp);
+		}
+
 		return true;
 	}
 	
