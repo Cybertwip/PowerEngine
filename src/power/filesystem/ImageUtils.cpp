19a43fda6aef19a327a86ecd4d2c9578f2104e4a
Kai <greentwip@gmail.com>
2024-10-22 09:01:10 +0200

    Primitive Dall-E integration

--- src/power/filesystem/ImageUtils.cpp
+++ src/power/filesystem/ImageUtils.cpp
@@ -1,7 +1,5 @@
 #include "ImageUtils.hpp"
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_MSC_SECURE_CRT
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
 #include "stb_image_write.h"
