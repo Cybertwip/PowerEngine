63c9862180fa598ffd75b1bd0121bd69f4097871
Kai <greentwip@gmail.com>
2024-10-22 12:37:07 +0200

    Fix crash on exit and fix image view prompt

--- src/power/graphics/drawing/MeshBatch.cpp
+++ src/power/graphics/drawing/MeshBatch.cpp
@@ -177,11 +177,7 @@ void MeshBatch::remove(std::reference_wrapper<Mesh> meshRef) {
 		size_t indexStartIdx = mMeshStartIndices[instanceId][meshIndex];
 		size_t indexCount = mesh.get_mesh_data().get_indices().size();
 		
-		size_t vertexStartIdx = mMeshVertexStartIndices[identifier][0];
-		size_t vertexEndIdx = (meshIndex + 1 < mMeshVertexStartIndices[identifier].size()) ?
-		mMeshVertexStartIndices[identifier][meshIndex + 1] :
-		mBatchPositions[identifier].size() / 3;
-		size_t vertexCount = vertexEndIdx - vertexStartIdx;
+		size_t vertexCount = mesh.get_mesh_data().get_vertices().size();
 		
 		// Remove indices
 		mBatchIndices[identifier].erase(mBatchIndices[identifier].begin() + indexStartIdx,
--- src/power/graphics/drawing/SkinnedMeshBatch.cpp
+++ src/power/graphics/drawing/SkinnedMeshBatch.cpp
@@ -197,11 +197,7 @@ void SkinnedMeshBatch::remove(std::reference_wrapper<SkinnedMesh> meshRef) {
 		size_t indexStartIdx = mMeshStartIndices[instanceId][meshIndex];
 		size_t indexCount = mesh.get_mesh_data().get_indices().size();
 		
-		size_t vertexStartIdx = mMeshVertexStartIndices[identifier][0];
-		size_t vertexEndIdx = (meshIndex + 1 < mMeshVertexStartIndices[identifier].size()) ?
-		mMeshVertexStartIndices[identifier][meshIndex + 1] :
-		mBatchPositions[identifier].size() / 3;
-		size_t vertexCount = vertexEndIdx - vertexStartIdx;
+		size_t vertexCount = mesh.get_mesh_data().get_skinned_vertices().size();
 		
 		// Remove indices
 		mBatchIndices[identifier].erase(mBatchIndices[identifier].begin() + indexStartIdx,
