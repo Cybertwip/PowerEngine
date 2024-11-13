f16bffddd4fb77d1feb8295c147aa9cf0dc5bd24
Kai <greentwip@cyber-navy.eu>
2024-11-13 21:30:28 +0100

    Style

--- src/power/execution/BlueprintManager.hpp
+++ src/power/execution/BlueprintManager.hpp
@@ -14,10 +14,14 @@ public:
 		
 		mBlueprintButton = std::make_shared<nanogui::Button>(canvas, "", FA_FLASK);
 		
-		mBlueprintButton->set_size(nanogui::Vector2i(48, 48));
+		mBlueprintButton->set_fixed_size(nanogui::Vector2i(48, 48));
 		
+		mBlueprintButton->set_background_color(nanogui::Color(255, 255, 255, 255));
+
+		mBlueprintButton->set_text_color(nanogui::Color(0, 0, 255, 255));
+
 		// Position the button in the lower-right corner
-		mBlueprintButton->set_position(nanogui::Vector2i(mCanvas.fixed_width() * 0.5f - mBlueprintButton->width() * 0.5f, mCanvas.fixed_height() - mBlueprintButton->height() - 10));
+		mBlueprintButton->set_position(nanogui::Vector2i(mCanvas.fixed_width() * 0.5f - mBlueprintButton->fixed_width() * 0.5f, mCanvas.fixed_height() - mBlueprintButton->fixed_height() - 20));
 		
 		mBlueprintButton->set_callback([this]() {
 		});
