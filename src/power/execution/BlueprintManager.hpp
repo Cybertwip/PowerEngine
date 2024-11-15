f0ba00e84634c3a93984b62f07b270f01cce613a
Kai <greentwip@cyber-navy.eu>
2024-11-15 06:48:52 +0100

    Add pin and data columns

--- src/power/execution/BlueprintManager.hpp
+++ src/power/execution/BlueprintManager.hpp
@@ -201,22 +201,21 @@ public:
 	StringNode(nanogui::Widget& parent, const std::string& title, nanogui::Vector2i size)
 	: nanogui::Window(parent, title) {
 		set_fixed_size(size);
-		
 		// Main window layout: Horizontal orientation, fill space
-		set_layout(std::make_unique<nanogui::GroupLayout>(0, 0));
+		set_layout(std::make_unique<nanogui::GridLayout>(nanogui::Orientation::Horizontal, 3, nanogui::Alignment::Minimum));
 		
 		// Left column placeholder (no pins)
 		mLeftColumn = std::make_unique<nanogui::Widget>(*this);
-		mLeftColumn->set_layout(std::make_unique<nanogui::BoxLayout>(nanogui::Orientation::Vertical, nanogui::Alignment::Minimum, 10, 15));
-		
+		mLeftColumn->set_layout(std::make_unique<nanogui::BoxLayout>(nanogui::Orientation::Vertical, nanogui::Alignment::Minimum, 0, 0)));
+
 		// Middle column for the node data
 		mDataColumn = std::make_unique<nanogui::Widget>(*this);
 		mDataColumn->set_layout(std::make_unique<nanogui::BoxLayout>(nanogui::Orientation::Vertical, nanogui::Alignment::Minimum, 10, 15));
 		
 		// Right column for output pins: Aligned to the right edge
 		mRightColumn = std::make_unique<nanogui::Widget>(*this);
-		mRightColumn->set_layout(std::make_unique<nanogui::BoxLayout>(nanogui::Orientation::Vertical, nanogui::Alignment::Minimum, 10, 15));
-		
+		mRightColumn->set_layout(std::make_unique<nanogui::BoxLayout>(nanogui::Orientation::Vertical, nanogui::Alignment::Minimum, 0, 0)));
+
 		// Text box inside the node data wrapper: Will be centered due to the vertical alignment
 		mTextBox = std::make_unique<nanogui::TextBox>(*mDataColumn, "");
 		mTextBox->set_editable(true);
