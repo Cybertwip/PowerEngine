3e58adb4da1b02da25a57553e021d573044d708c
Kai <greentwip@cyber-navy.eu>
2024-11-17 08:40:31 +0100

    Add transparent widget for window drag events

--- src/power/execution/BlueprintNode.hpp
+++ src/power/execution/BlueprintNode.hpp
@@ -82,17 +82,41 @@ public:
 private:
 	bool mouse_button_event(const nanogui::Vector2i &p, int button, bool down,
 									int modifiers) override {
-			
 		mWindow.mouse_button_event(p, button, down, modifiers);
 		
-		bool handled = nanogui::Widget::mouse_button_event(p, button, down, modifiers);
-
-		return handled;
+		return nanogui::Widget::mouse_button_event(p, button, down, modifiers);
 	}
 	
 	bool mouse_drag_event(const nanogui::Vector2i &p, const nanogui::Vector2i &rel, int button, int modifiers) override {
 		mWindow.mouse_drag_event(p, rel, button, modifiers);
+		return nanogui::Widget::mouse_drag_event(p, rel, button, modifiers);
+	}
+	
+	bool keyboard_event(int key, int scancode, int action, int modifiers) override {
+		return mWindow.keyboard_event(key, scancode, action, modifiers);
+	}
+	
+	nanogui::Window& mWindow;
+};
+
+
+class PassThroughButton : public nanogui::Button {
+public:
+	PassThroughButton(nanogui::Window& parent, const std::string &caption = "", int icon = 0)
+	: nanogui::Button(parent, caption, icon), mWindow(parent) {
 		
+	}
+	
+private:
+	bool mouse_button_event(const nanogui::Vector2i &p, int button, bool down,
+							int modifiers) override {
+		mWindow.mouse_button_event(p, button, down, modifiers);
+		
+		return nanogui::Widget::mouse_button_event(p, button, down, modifiers);
+	}
+	
+	bool mouse_drag_event(const nanogui::Vector2i &p, const nanogui::Vector2i &rel, int button, int modifiers) override {
+		mWindow.mouse_drag_event(p, rel, button, modifiers);
 		return nanogui::Widget::mouse_drag_event(p, rel, button, modifiers);
 	}
 	
