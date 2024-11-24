0d692fcb70bb49025044ab1f8838778a29550e0c
Kai <greentwip@cyber-navy.eu>
2024-11-24 22:01:21 +0100

    Persist key press node

--- src/power/execution/KeyPressNode.hpp
+++ src/power/execution/KeyPressNode.hpp
@@ -5,7 +5,9 @@
 #include <nanogui/button.h>
 
 #include <memory>
+#include <optional>
 #include <string>
+#include <variant>
 
 namespace blueprint {
 class BlueprintCanvas;
@@ -14,6 +16,20 @@ class KeyPressNode : public BlueprintNode {
 public:
 	KeyPressNode(std::optional<std::reference_wrapper<BlueprintCanvas>> parent, nanogui::Vector2i size, std::function<int()> id_registrator_lambda);
 	
+	virtual std::optional<std::variant<Entity, std::string, int, float, bool>> get_data() {
+		return mKeyCode;
+	}
+	
+	virtual void set_data(std::optional<std::variant<Entity, std::string, int, float, bool>> data) {
+		if (data.has_value()) {
+			mKeyCode = std::get<int>(data.value());
+			mConfigured = true;
+		} else {
+			mKeyCode = -1;
+			mConfigured = false;
+		}
+	}
+
 private:
 	bool keyboard_event(int key, int scancode, int action, int modifiers) override;
 
