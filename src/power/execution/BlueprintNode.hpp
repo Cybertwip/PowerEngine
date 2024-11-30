bce3d63f23f7b9a14f63b8a7f7a9612e556cdd44
Cybertwip <greentwip@cyber-navy.eu>
2024-11-30 23:13:51 +0100

    Better event delegation

--- src/power/execution/BlueprintNode.hpp
+++ src/power/execution/BlueprintNode.hpp
@@ -131,15 +131,13 @@ public:
 private:
 	bool mouse_button_event(const nanogui::Vector2i &p, int button, bool down,
 									int modifiers) override {
-		nanogui::Widget::mouse_button_event(p, button, down, modifiers);
-		// delegate and propagate event
-		return false;
+		mWindow.mouse_button_event(p, button, down, modifiers);
+		return nanogui::Widget::mouse_button_event(p, button, down, modifiers);
 	}
 	
 	bool mouse_drag_event(const nanogui::Vector2i &p, const nanogui::Vector2i &rel, int button, int modifiers) override {
-		nanogui::Widget::mouse_drag_event(p, rel, button, modifiers);
-		// delegate and propagate event
-		return false;
+		mWindow.mouse_drag_event(p, rel, button, modifiers);
+		return nanogui::Widget::mouse_drag_event(p, rel, button, modifiers);
 	}
 	
 	bool keyboard_event(int key, int scancode, int action, int modifiers) override {
