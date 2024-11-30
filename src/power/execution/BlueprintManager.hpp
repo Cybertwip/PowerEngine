8b4f408c97e6942aa2b06a5c536c06bb34efaa10
Cybertwip <greentwip@cyber-navy.eu>
2024-11-30 03:01:02 +0100

    Fix passthrough events

--- src/power/execution/BlueprintManager.hpp
+++ src/power/execution/BlueprintManager.hpp
@@ -58,14 +58,14 @@ public:
 	
 	// Override mouse_button_event to consume the event
 	bool mouse_button_event(const nanogui::Vector2i &p, int button, bool down, int modifiers) override {
-		nanogui::ScenePanel::mouse_button_event(p, button, down, modifiers);
+		ScenePanel::mouse_button_event(p, button, down, modifiers);
 		
 		return true;
 	}
 	
 	// Override mouse_motion_event to consume the event
 	bool mouse_motion_event(const nanogui::Vector2i &p, const nanogui::Vector2i &rel, int button, int modifiers) override {
-		nanogui::ScenePanel::mouse_motion_event(p, rel, button, modifiers);
+		ScenePanel::mouse_motion_event(p, rel, button, modifiers);
 		
 		return true;
 	}
